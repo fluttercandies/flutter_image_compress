c7f42fd369600aa484e4433a6c12532a3486eb5b
cjl_macbook <cjl_spy@163.com>
2018-10-22 22:55:42 +0800

    add rotate params
    and update android code

--- android/src/main/kotlin/com/example/flutterimagecompress/BitmapCompressExt.kt
+++ android/src/main/kotlin/com/example/flutterimagecompress/BitmapCompressExt.kt
@@ -1,9 +1,10 @@
 package com.example.flutterimagecompress
 
 import android.graphics.Bitmap
+import android.graphics.Matrix
 import java.io.ByteArrayOutputStream
 
-fun Bitmap.compress(minWidth: Int, minHeight: Int, quality: Int): ByteArray {
+fun Bitmap.compress(minWidth: Int, minHeight: Int, quality: Int, rotate: Int = 0): ByteArray {
     val baos = ByteArrayOutputStream()
 
     val w = this.width
@@ -20,6 +21,20 @@ fun Bitmap.compress(minWidth: Int, minHeight: Int, quality: Int): ByteArray {
     val destW = w / scale
     val destH = h / scale
 
-    Bitmap.createScaledBitmap(this, destW, destH, true).compress(Bitmap.CompressFormat.JPEG, quality, baos)
+    Bitmap.createScaledBitmap(this, destW, destH, true)
+            .rotate(rotate)
+            .compress(Bitmap.CompressFormat.JPEG, quality, baos)
+
     return baos.toByteArray()
+}
+
+fun Bitmap.rotate(rotate: Int): Bitmap {
+    if (rotate % 360 != 0) {
+        val matrix = Matrix()
+        matrix.setRotate(rotate.toFloat())
+        // 围绕原地进行旋转
+        return Bitmap.createBitmap(this, 0, 0, width, height, matrix, false)
+    } else {
+        return this
+    }
 }
--- lib/flutter_image_compress.dart
+++ lib/flutter_image_compress.dart
@@ -8,30 +8,62 @@ class FlutterImageCompress {
   static const MethodChannel _channel =
       const MethodChannel('flutter_image_compress');
 
-  static Future<List<int>> compressWithList(List<int> image,
-      {int minWidth = 1920, int minHeight = 1080, int quality = 95}) async {
-    final result = await _channel.invokeMethod(
-        "compressWithList", [image, minWidth, minHeight, quality]);
+  static Future<List<int>> compressWithList(
+    List<int> image, {
+    int minWidth = 1920,
+    int minHeight = 1080,
+    int quality = 95,
+    int rotate = 0,
+  }) async {
+    final result = await _channel.invokeMethod("compressWithList", [
+      image,
+      minWidth,
+      minHeight,
+      quality,
+      rotate,
+    ]);
 
     return convertDynamic(result);
   }
 
-  static Future<List<int>> compressWithFile(String path,
-      {int minWidth = 1920, int minHeight = 1080, int quality = 95}) async {
-    final result = await _channel
-        .invokeMethod("compressWithFile", [path, minWidth, minHeight, quality]);
+  static Future<List<int>> compressWithFile(
+    String path, {
+    int minWidth = 1920,
+    int minHeight = 1080,
+    int quality = 95,
+    int rotate = 0,
+  }) async {
+    final result = await _channel.invokeMethod("compressWithFile", [
+      path,
+      minWidth,
+      minHeight,
+      quality,
+      rotate,
+    ]);
     return convertDynamic(result);
   }
 
-  static Future<File> compressAndGetFile(String path, String targetPath,
-      {int minWidth = 1920, int minHeight = 1080, int quality = 95}) async {
+  static Future<File> compressAndGetFile(
+    String path,
+    String targetPath, {
+    int minWidth = 1920,
+    int minHeight = 1080,
+    int quality = 95,
+    int rotate = 0,
+  }) async {
     if (!File(path).existsSync()) {
       return null;
     }
 
-    final String result = await _channel.invokeMethod(
-        "compressWithFileAndGetFile",
-        [path, minWidth, minHeight, quality, targetPath]);
+    final String result =
+        await _channel.invokeMethod("compressWithFileAndGetFile", [
+      path,
+      minWidth,
+      minHeight,
+      quality,
+      targetPath,
+      rotate,
+    ]);
 
     return File(result);
   }
@@ -41,6 +73,7 @@ class FlutterImageCompress {
     int minWidth = 1920,
     int minHeight = 1080,
     int quality = 95,
+    int rotate = 0,
   }) async {
     var img = AssetImage(assetName);
     var config = new ImageConfiguration();
