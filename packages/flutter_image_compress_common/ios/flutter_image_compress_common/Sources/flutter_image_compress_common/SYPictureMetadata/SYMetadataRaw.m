9ced076e822bf639ea1e474af767c13df87a14fc
alejandro-all-win-software <alejandro@all-win.software>
2025-07-07 10:05:42 -0600

    chore: added ios spm support

---
+++ packages/flutter_image_compress_common/ios/flutter_image_compress_common/Sources/flutter_image_compress_common/SYPictureMetadata/SYMetadataRaw.m
@@ -0,0 +1,13 @@
+//
+//  SYMetadataRaw.m
+//  SYPictureMetadataExample
+//
+//  Created by Stan Chevallier on 12/16/12.
+//  Copyright (c) 2012 Syan. All rights reserved.
+//
+
+#import "../include/flutter_image_compress_common/SYPictureMetadata/SYMetadataRaw.h"
+
+@implementation SYMetadataRaw
+
+@end
